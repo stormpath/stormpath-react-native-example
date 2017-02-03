7d5484e95c8f7a369d4b56c0ad5acebe627627db
kewp <karlpenzhorn@gmail.com>
2017-02-03 12:11:20 +0200

    Screen record script

---
+++ record.bat
@@ -0,0 +1,10 @@
+REM Doesn't work on emulator
+adb shell rm /sdcard/demo*.mp4
+adb shell screenrecord --time 4 /sdcard/demo.mp4 --verbose
+REM weird output name issue
+adb shell mv /sdcard/demo*.mp4 /sdcard/demo.mp4
+del demo.mp4
+adb pull /sdcard/demo.mp4 .
+del demo.gif
+ffmpeg -i demo.mp4 -vf fps=15,scale=320:-1:flags=lanczos,palettegen palette.png
+ffmpeg -i demo.mp4 -i palette.png -filter_complex "fps=15,scale=400:-1:flags=lanczos[x];[x][1:v]paletteuse" demo.gif
