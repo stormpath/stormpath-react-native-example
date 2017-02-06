f2649d91fc19d38efff868706b4380ad0069d7e7
kewp <karlpenzhorn@gmail.com>
2017-02-06 16:48:06 +0200

    Login page networking

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,10 +4,67 @@ import {
 	Text,
 	TextInput,
 	View,
-	Button
+	Button,
+	Alert,
+	ActivityIndicator
 } from 'react-native';
 
 export default class Login extends Component {
+
+	  state = {
+	  	username: '',
+	  	password: '',
+	    isLoggingIn: false,
+	    message: ''
+	  }
+
+	_userLogin = () => { 
+
+		this.setState({isLoggingIn: true, message:''});
+
+		var params = {
+		    username: this.state.username,
+		    password: this.state.password,
+		    grant_type: 'password'
+		};
+
+		var formBody = [];
+		for (var property in params) {
+		  var encodedKey = encodeURIComponent(property);
+		  var encodedValue = encodeURIComponent(params[property]);
+		  formBody.push(encodedKey + "=" + encodedValue);
+		}
+		formBody = formBody.join("&");
+
+		var proceed = false;
+	    fetch("https://big-jelly.apps.stormpath.io/oauth/token", {
+	        method: "POST", 
+	        headers: {
+	          'Content-Type': 'application/x-www-form-urlencoded'
+	        },
+	        body: formBody
+	      })
+	      .then((response) => response.json())
+	      .then((response) => {
+	      	if (response.error) this.setState({message: response.message});
+	      	else proceed = true;
+	      })
+	      .then(() => {
+	      	this.setState({isLoggingIn: false})
+	      	if (proceed) this.props.onLoginPress();
+	      })
+	      .done();
+	} 
+  
+    clearUsername = () => { 
+    	this._username.setNativeProps({text: ''});
+    	this.setState({message: ''});
+    }
+	
+	clearPassword = () => { 
+    	this._password.setNativeProps({text: ''});
+    	this.setState({message: ''});
+    }
 
 	render() {
 		return (
@@ -16,11 +73,32 @@ export default class Login extends Component {
 					style={{fontSize: 27}}>
 					Login
 				</Text>
-				<TextInput placeholder='Username' />
-				<TextInput placeholder='Password' />
+				<TextInput
+					ref={component => this._username = component}
+					placeholder='Username' 
+					onChangeText={(username) => this.setState({username})}
+					autoFocus={true}
+					onFocus={this.clearUsername}
+				/>
+				<TextInput 
+					ref='{component => this._password = component}'
+					placeholder='Password' 
+					onChangeText={(password) => this.setState({password})}
+					secureTextEntry={true}
+					onFocus={this.clearPassword}
+					onSubmitEditing={this._userLogin}
+				/>
+				{!!this.state.message && (
+					<Text
+						style={{fontSize: 14, color: 'red', padding: 5}}>
+						{this.state.message}
+					</Text>
+				)}
+				{this.state.isLoggingIn && <ActivityIndicator />}
 				<View style={{margin:7}} />
 				<Button 
-		      		onPress={this.props.onLoginPress}
+					disabled={this.state.isLoggingIn||!this.state.username||!this.state.password}
+		      		onPress={this._userLogin}
 		      		title="Submit"
 		      	/>
 	      </ScrollView>
